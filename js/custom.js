b14ff2b59ade0564530cb63ce00ddcd324ef449b
Jakub Ludwa <jludwa@creatuity.pl>
2016-10-03 10:14:15 +0200

    [Chapter 5] Adding jQuery library, Google fonts and custom js file

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    // Top slider
+    $('#top-slider').carousel({
+        interval: 4000
+    });
+    // Promoslider
+    $('#promo-slider').carousel({
+        interval: 3000
+    });
+
+});
